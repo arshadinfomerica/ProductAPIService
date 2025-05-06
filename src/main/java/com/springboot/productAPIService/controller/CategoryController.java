9fe473df6eb9666904f9c668300a6b86e3a068b9
Arshad Afaque <arshad@infomericainc.in>
2025-05-06 15:35:33 +0530

    Create Controller for Product and Category

--- src/main/java/com/springboot/productAPIService/controller/CategoryController.java
+++ src/main/java/com/springboot/productAPIService/controller/CategoryController.java
@@ -1,7 +1,12 @@
 package com.springboot.productAPIService.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +28,22 @@ public class CategoryController {
 	@PostMapping
 	public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto) {
 		return new ResponseEntity<>(categoryService.createCategory(categoryDto),HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public List<CategoryDto> getAllCategory(){
+		return categoryService.getAllCategory();
+		
+	}
+	
+	@GetMapping("/{id}")
+	public CategoryDto getById(@PathVariable Long id) {
+		return categoryService.getById(id);
+	}
+	@DeleteMapping("/{id}")
+	public String deleteCategory(@PathVariable Long id) {
+		return categoryService.deleteCategory(id);
+		
 	}
 
 }
--- src/main/java/com/springboot/productAPIService/controller/ProductController.java
+++ src/main/java/com/springboot/productAPIService/controller/ProductController.java
@@ -1,10 +1,55 @@
 package com.springboot.productAPIService.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import com.springboot.productAPIService.dto.ProductDto;
+import com.springboot.productAPIService.service.ProductService;
+
+import lombok.AllArgsConstructor;
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
+	
+	private ProductService productService;
+	
+	
+	@PostMapping
+	public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto) {
+		return  new ResponseEntity<>(productService.createProduct(productDto),HttpStatus.CREATED);
+	}
+	
+	
+	@GetMapping
+	public List<ProductDto> getAllProduct(){
+		return productService.getAllProduct();
+	}
+	
+	@GetMapping("/{id}")
+	public ProductDto getById(@PathVariable Long id) {
+		return productService.getById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public ProductDto updateProductByid(@PathVariable Long id,@RequestBody ProductDto productDto) {
+		return productService.updateById(id, productDto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteById(@PathVariable Long id) {
+		return productService.deleteproduct(id);
+	}
 
 }
